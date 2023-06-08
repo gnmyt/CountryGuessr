1e244047e846340cc8dfbe7308c0a463ea2f451e
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-09 00:02:58 +0200

    Created the ActionCard.jsx

---
+++ webui/src/pages/Home/components/ActionCard/ActionCard.jsx
@@ -0,0 +1,11 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import "./styles.sass";
+
+export const ActionCard = ({ title, icon, onClick }) => {
+  return (
+    <div className="action-card" onClick={onClick}>
+        <FontAwesomeIcon icon={icon} />
+        <h1>{title}</h1>
+    </div>
+  );
+}
