e57b2bb6639fc2d0e47f4071eed697dc04e50438
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-09 00:05:56 +0200

    Updated the Home.jsx

--- webui/src/pages/Home/Home.jsx
+++ webui/src/pages/Home/Home.jsx
@@ -1,10 +1,16 @@
 import PageTitle from "@/common/components/PageTitle";
 import "./styles.sass";
+import ActionCard from "@/pages/Home/components/ActionCard";
+import {faArrowRightToBracket, faPlusSquare} from "@fortawesome/free-solid-svg-icons";
 
 export const Home = () => {
     return (
         <div className="home-page">
             <PageTitle />
+            <div className="action-area">
+                <ActionCard title="Raum beitreten" icon={faArrowRightToBracket} onClick={() => {}} />
+                <ActionCard title="Raum erstellen" icon={faPlusSquare} onClick={() => {}} />
+            </div>
         </div>
     )
 }
