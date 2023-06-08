4459e0b2603a394d6a2ef83fc047260a49cc434a
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-08 22:34:08 +0200

    Added the BlurredBackground to the main.jsx

--- webui/src/main.jsx
+++ webui/src/main.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "@/common/styles/main.sass";
+import BlurredBackground from "@/common/components/BlurredBackground";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+      <BlurredBackground />
+
+      <div className="app">
+          <App />
+      </div>
+
   </React.StrictMode>,
 );
