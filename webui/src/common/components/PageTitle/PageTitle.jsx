87b49f9ed92aad5fd853680b2cf184967beaf59c
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-09 02:21:23 +0200

    Updated the PageTitle.jsx

--- webui/src/common/components/PageTitle/PageTitle.jsx
+++ webui/src/common/components/PageTitle/PageTitle.jsx
@@ -1,10 +1,16 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faBookAtlas} from "@fortawesome/free-solid-svg-icons";
 import "./styles.sass";
+import {useContext} from "react";
+import {StateContext} from "@/common/contexts/StateContext";
 
-export const PageTitle = () => (
-    <div className="page-title">
-        <FontAwesomeIcon icon={faBookAtlas} />
-        <h1>CountryGuessr</h1>
-    </div>
-)
+export const PageTitle = ({allowBack = false}) => {
+    const {setCurrentState} = useContext(StateContext);
+
+    return (
+        <div className={"page-title" + (allowBack ? " title-clickable" : "")} onClick={() => allowBack ? setCurrentState("Home") : ""}>
+            <FontAwesomeIcon icon={faBookAtlas}/>
+            <h1>CountryGuessr</h1>
+        </div>
+    );
+}
