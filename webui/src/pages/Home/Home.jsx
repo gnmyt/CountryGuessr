57c5ecb41e1887a9e4deb11e1b83ef034802577c
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-09 02:23:37 +0200

    Updated the Home.jsx

--- webui/src/pages/Home/Home.jsx
+++ webui/src/pages/Home/Home.jsx
@@ -2,15 +2,19 @@ import PageTitle from "@/common/components/PageTitle";
 import "./styles.sass";
 import ActionCard from "@/pages/Home/components/ActionCard";
 import {faArrowRightToBracket, faPlusSquare} from "@fortawesome/free-solid-svg-icons";
+import {StateContext} from "@/common/contexts/StateContext";
+import {useContext} from "react";
 
 export const Home = () => {
+    const {setCurrentState} = useContext(StateContext);
+
     return (
         <div className="home-page">
             <PageTitle />
             <div className="action-area">
                 <ActionCard title="Raum beitreten" icon={faArrowRightToBracket} onClick={() => {}} />
-                <ActionCard title="Raum erstellen" icon={faPlusSquare} onClick={() => {}} />
+                <ActionCard title="Raum erstellen" icon={faPlusSquare} onClick={() => setCurrentState("GameCreation")} />
             </div>
         </div>
-    )
+    );
 }
