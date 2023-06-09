c9404be915edd50ea7e221200500d02bb69327fc
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-09 03:14:02 +0200

    Created the LegalArea.jsx

---
+++ webui/src/common/components/LegalArea/LegalArea.jsx
@@ -0,0 +1,8 @@
+import "./styles.sass";
+
+export const LegalArea = () => (
+    <div className="legal-area">
+        <a href="https://gnmyt.dev/privacy" target="_blank">Datenschutz</a>
+        <a href="https://gnmyt.dev/imprint" target="_blank">Impressum</a>
+    </div>
+)
