6ed10e348dcb436e5f4bd06df973bb671a87678b
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-09 02:24:05 +0200

    Integrated the StateProvider into the main.jsx

--- webui/src/main.jsx
+++ webui/src/main.jsx
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "@/common/styles/main.sass";
 import BlurredBackground from "@/common/components/BlurredBackground";
+import {StateProvider} from "@/common/contexts/StateContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-      <BlurredBackground />
+    <React.StrictMode>
+        <BlurredBackground/>
 
-      <div className="app">
-          <App />
-      </div>
+        <div className="app">
+            <StateProvider>
+                <App/>
+            </StateProvider>
+        </div>
 
-  </React.StrictMode>,
+    </React.StrictMode>,
 );
