bfa011d1ec2b6b172ab0472408d03932650ae649
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-09 03:02:09 +0200

    Created the Game.jsx

---
+++ webui/src/pages/Game/Game.jsx
@@ -0,0 +1,9 @@
+import "./styles.sass";
+
+export const Game = () => {
+    return (
+        <div className="active-wrapper">
+            <h2 style={{color: "#FFFFFF"}}>ingame state</h2>
+        </div>
+    )
+}
